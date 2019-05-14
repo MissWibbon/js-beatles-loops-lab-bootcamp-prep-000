ab338685afe396b58d76c2d022a3c167424886c8
Michelle Wuebben <wuebbenmichelle@gmail.com>
2019-05-14 20:16:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-function theBeatlesPlay(musicians, instruments) {
-  var array = [musicians, instruments];
-  
-  for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i]);
-  }
-
-  return array;
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
 }
 
 function johnLennonFacts(facts) {
